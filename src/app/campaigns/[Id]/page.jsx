2370d6cd0eef3b906c65a8bd13e4a6ba1833dc48
offxsagr <hakies.06@gmail.com>
2024-11-18 01:47:32 +0530

    router added

--- src/app/campaigns/[Id]/page.jsx
+++ src/app/campaigns/[Id]/page.jsx
@@ -266,11 +266,11 @@ const CampaignPage = () => {
         <div className="p-6 bg-green-100 text-green-700 rounded-lg mt-4">
           <p>Join the Oohpoint Official WhatsApp Channel to keep yourself updated and offer you exclusive discounts!</p>
           <button
-            onClick={() => window.open(firstVendor.whatsapp, '_blank', 'noopener noreferrer')}
+            onClick={() => router.push(firstVendor.whatsapp)}
             className="mt-2 text-white bg-green-600 hover:bg-green-700 font-medium rounded-lg px-4 py-2"
           >
             Join Now
-            </button>
+          </button>
         </div>
 
         {/* Verified Vendor */}
