4358dcac2c0906125e7f37150b0929afd0ddcb4f
offxsagr <hakies.06@gmail.com>
2024-11-18 13:23:23 +0530

    fixesd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
-import { Metadata } from "next";
+import type { Metadata } from "next";
 import localFont from "next/font/local";
 import Footer from "./components/Footer"; // Adjust path if necessary
 import Sidebar from "./components/Sidebar"; // Import Sidebar component
 import "./globals.css";
-import Head from "next/head"; // Import Head component for setting favicon
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -19,7 +18,8 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "OHH POINT",
   description: "",
-  // Remove the favicon from here
+  icons: '/logo.png', // Favicon for the app
+  
 };
 
 export default function RootLayout({
@@ -29,19 +29,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
-        <Head>
-          {/* Set favicon here */}
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-
         {/* Sidebar */}
         <Sidebar /> {/* Add Sidebar here */}
 
         {/* Main content area */}
-        <main className="flex-grow lg:ml-20">
+        <main className="flex-grow lg:ml-20"> {/* Adjust margin-left based on sidebar size */}
           {children}
         </main>
 
