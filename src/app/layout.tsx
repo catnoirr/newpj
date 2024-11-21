843d07061f4d62877098739c8cea53262afceab3
offxsagr <hakies.06@gmail.com>
2024-11-21 23:59:39 +0530

    fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import Footer from "./components/Footer"; // Adjust path if necessary
-import Sidebar from "./components/Sidebar"; // Import Sidebar component
+// import Sidebar from "./components/Sidebar"; // Import Sidebar component
 import "./globals.css";
 import ResponsiveMenu from "./components/ResponsiveMenu";
 
