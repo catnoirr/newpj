35464f56f40c827753059f6448f9d75dcb760d31
offxsagr <hakies.06@gmail.com>
2024-11-25 09:20:28 +0530

    advertise done

---
+++ src/app/advertise/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import FrontPage from './components/FrontPage';
+import Features from './components/Feature';
+import Review from './components/ReviewPage';
+
+const JoinComunity = () => {
+    return (
+        <div>
+           
+            <FrontPage />
+            <Features />
+            <Review />
+        </div>
+    );
+};
+
+export default JoinComunity;
