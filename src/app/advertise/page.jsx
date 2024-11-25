0d939a5cd676767dd7bf5df192360671a78924e4
offxsagr <hakies.06@gmail.com>
2024-11-25 09:55:14 +0530

    advertise done

--- src/app/advertise/page.jsx
+++ src/app/advertise/page.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import FrontPage from './components/FrontPage';
 import Features from './components/Feature';
 import Review from './components/ReviewPage';
+import FAQ from './components/faq';
+import NewWay from './components/NewWay';
+import WorkWithUs from "./components/WorkWithUs";
 
 const JoinComunity = () => {
     return (
@@ -10,6 +13,10 @@ const JoinComunity = () => {
             <FrontPage />
             <Features />
             <Review />
+            <NewWay />
+            <FAQ />
+            <WorkWithUs />
+
         </div>
     );
 };
